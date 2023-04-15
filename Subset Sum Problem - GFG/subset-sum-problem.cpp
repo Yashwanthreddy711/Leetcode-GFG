4d1741f9a4a1c772fec2e91c74b1f9c347e4e572
Yashwanth Reddy <99392805+Yashwanthreddy711@users.noreply.github.com>
2023-04-15 08:57:47 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -37,8 +37,29 @@ bool mychamp(int ind,int target,vector<int>arr,vector<vector<int>>&dp)
         // code here 
     
         int n=arr.size();
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        return mychamp(n-1,sum,arr,dp);
+        //vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        //return mychamp(n-1,sum,arr,dp);
+        vector<vector<bool>>dp(n,vector<bool>(sum+1,0));
+        for(int i=0;i<n;i++)
+        {
+            dp[i][0]=true;
+        }
+        dp[0][arr[0]]=true;
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int target=1;target<=sum;target++)
+            {
+                bool pick = false;
+                if(arr[ind]<=target)
+                 {
+                    pick=dp[ind-1][target-arr[ind]];
+                  }
+    
+              bool nonpick=dp[ind-1][target];
+              dp[ind][target]= pick || nonpick;
+            }
+        }
+        return dp[n-1][sum];
     }
 };
 
