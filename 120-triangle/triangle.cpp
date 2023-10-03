706a21d30e7bc0a133ac6de99f1e6ee61b98c5d7
Yashwanth Reddy <99392805+Yashwanthreddy711@users.noreply.github.com>
2023-10-03 19:51:47 +0530

    Time: 8 ms (29.34%) | Memory: 9.3 MB (17.24%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -12,8 +12,17 @@ public:
     int minimumTotal(vector<vector<int>>& nums) {
         int n=nums.size();
         
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-         return recursion(0,0,nums,dp); 
-         
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+           dp[n-1][i]=nums[n-1][i];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int one=nums[i][j]+dp[i+1][j];
+                int two=nums[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(one,two);
+            }
+        }
+         return dp[0][0];
     }
 };
