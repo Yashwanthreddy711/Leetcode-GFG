91d23591c43a3a0573ec2839bd43879ddf60a5a0
Yashwanth Reddy <99392805+Yashwanthreddy711@users.noreply.github.com>
2023-10-03 19:42:03 +0530

    Time: 3 ms (88.10%) | Memory: 9.4 MB (11.03%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int recursion(int row,int col,vector<vector<int>>& nums,vector<vector<int>>&dp)
+    {
+        int n=nums.size();
+        if(row==n-1)return nums[row][col];
+        if(dp[row][col]!=-1)return dp[row][col];
+        int one=recursion(row+1,col,nums,dp)+nums[row][col];
+        int two=recursion(row+1,col+1,nums,dp)+nums[row][col];
+        return dp[row][col]=min(one,two);
+    }
+    int minimumTotal(vector<vector<int>>& nums) {
+        int n=nums.size();
+        
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+         return recursion(0,0,nums,dp); 
+         
+    }
+};
