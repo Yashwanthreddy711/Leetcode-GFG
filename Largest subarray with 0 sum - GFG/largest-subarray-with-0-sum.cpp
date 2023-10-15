63b829206845a21c797e78513d83f79c0cd00a62
Yashwanth Reddy <99392805+Yashwanthreddy711@users.noreply.github.com>
2023-10-15 18:45:43 +0530

    Added solution - LeetHub

--- Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
@@ -10,50 +10,26 @@ using namespace std;
 
 class Solution{
     public:
-    // void recursion(vector<int>&A,vector<int>&v,int n,int index,int &max,int target,int sum)
-    // {
-    //     if(sum==target)
-    //     {
-    //         if(v.size()>max)
-    //       {
-    //           max=v.size();
-    //       }
-    //         return;
-    //     }
-    //     if(index==n)
-    //     {
-    //       return;
-    //     }
-    //     v.push_back(A[index]);
-    //     recursion(A,v,n,index+1,max,target,sum-A[index]);
-    //     v.pop_back();
-    // }
-   int maxLen(vector<int>& A, int n)
-{  
-    map<int,int> mp;
-    int maxLength = 0;
-    int sum = 0;
-    for(int i = 0; i < n; i++)
-    {
-        sum += A[i];
-        if (sum == 0) {
-            maxLength = i + 1;
-        } else if (mp.find(sum) != mp.end()) {
-            maxLength = max(maxLength, i - mp[sum]);
-        } else {
-            mp[sum] = i;
+    int maxLen(vector<int>&nums, int n)
+    {   
+        unordered_map<int,int>mp;
+        int maxi=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum==0){
+                maxi=i+1;
+            }
+            else {
+                if(mp.find(sum)!=mp.end()){
+                     maxi=max(maxi,i-mp[sum]);  
+                }
+                else {
+                    mp[sum]=i;
+                }
+            }
         }
-    }
-    return maxLength;
-
-
-        // int index=0;
-        // int max=0;
-        // int target=0;
-        // int sum=0;
-        // vector<int>v;
-        // recursion(A,v,n-1,index,max,target,sum);
-        // return max;
+        return maxi;
     }
 };
 
