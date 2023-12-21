7725da7b226f9531f0d1c637480eb322871eaeb0
Yashwanth Reddy <99392805+Yashwanthreddy711@users.noreply.github.com>
2023-12-21 13:00:26 +0530

    Time: 52 ms (40.59%) | Memory: 42 MB (18.40%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    // var ans="Hello World";
+     return ()=>"Hello World";
+    // return function(...args) {
+    //     return ans;
+    // }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
