2ca1eff2bd40ddaaca00b01279a0931f4d7345da
Yashwanth Reddy <99392805+Yashwanthreddy711@users.noreply.github.com>
2024-04-01 09:55:40 +0530

    Time: 4 ms (13.26%) | Memory: 7.9 MB (17.69%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
-  int recursion(int row, int col, vector<vector<int>>& grid,vector<vector<int>>&dp) {
-    if (row < 0 || col < 0) {
-        return 0;
-    }
-    if (row == 0 || col == 0) {
+int recursion(int row,int col,int m,int n,vector<vector<int>>&nums,vector<vector<int>>&dp){
+    if(row==m-1 && col==n-1){
         return 1;
     }
-    if(dp[row][col]!=-1){
-        return dp[row][col];
-    }
-    int right = recursion(row, col - 1, grid,dp);
-    int down = recursion(row - 1, col, grid,dp);
-    return dp[row][col]=right + down;
+    if(row>=m || col>=n)return 0;
+    if(dp[row][col]!=-1)return dp[row][col];
+    int bottom=recursion(row+1,col,m,n,nums,dp);
+    int right=recursion(row,col+1,m,n,nums,dp);
+    return dp[row][col]=bottom+right;
 }
-
-int uniquePaths(int m, int n) {
-    vector<vector<int>>dp(m,vector<int>(n,0));
-    for(int i=0;i<m;i++){
-      dp[i][0]=1;
+    int uniquePaths(int m, int n) { 
+        vector<vector<int>>nums(m,vector<int>(n));
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return recursion(0,0,m,n,nums,dp);
     }
-    for(int i=0;i<n;i++){
-        dp[0][i]=1;
-    }
-    
-    for(int row=1;row<m;row++){
-        for(int col=1;col<n;col++){
-            int right=dp[row][col-1];
-            int down=dp[row-1][col];
-            dp[row][col]=right+down;
-        }
-    }
-    return dp[m-1][n-1];
-}
-
 };
