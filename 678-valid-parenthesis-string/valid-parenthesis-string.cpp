fa75110a41773878ded9cbb450e8c072a7e2d949
Yashwanth Reddy <99392805+Yashwanthreddy711@users.noreply.github.com>
2024-04-07 21:46:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (75.32%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+     stack<int>star;
+     stack<int>st;
+     for(int i=0;i<s.size();i++){
+        if(s[i]=='('){
+            st.push(i);
+        }
+        else if(s[i]=='*'){
+            star.push(i);
+        }
+        else if(s[i]==')'){
+            if(st.empty() && star.empty())return false;
+            else if(!st.empty())st.pop();
+            else star.pop();
+        }
+     }
+     while(!st.empty()){
+        if(star.empty())return false;
+        if(st.top()>star.top())return false;
+        st.pop();
+        star.pop();
+     }
+     return true;
+    
+
+    }
+};
